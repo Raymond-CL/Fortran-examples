526600ecd77b4176b39d36d927b3fcf3c5524017
Raymond Chen Lin <raymond.chen88@outlook.com>
2022-08-23 23:49:16 +0800

    updated readme

--- fort-ex-buffon/main.f90
+++ fort-ex-buffon/main.f90
@@ -1,13 +1,5 @@
 program main
 
-! Buffon's needle - geometric probability experiment
-! lines are parallel to x-axis, with separation t (position of the needle on x-axis is irrelevant)
-! needle length: l, with position of the mid-point on the y-axis: y, and orientation angle a
-! generate random needle: y=[0,t] and a=[0,PI]
-! n_tot: number of needles dropped
-! n_hit: number of needles that crossed the line
-! A Monte-Carlo simulation to estimate the value of PI
-
       use nrtype
       use nr, only: ran0_v,ran1_v,ran2_v
       IMPLICIT NONE
