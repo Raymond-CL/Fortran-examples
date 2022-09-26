95a9f9f015ce308ffb234410806a7a59ee65ad3c
Raymond Chen Lin <raymond.chen88@outlook.com>
2022-09-27 00:03:51 +0800

    added gaussian quadrature

---
+++ fort-ex-gauss/main.f90
@@ -0,0 +1,69 @@
+program main
+use gauss
+implicit none
+integer(1) :: i
+real(dp) :: xmin,xmax,total,xi(n),wi(n)
+xmin = 0d0
+xmax = 100d0
+
+
+
+! Gauss-Legendre
+call gleg(xmin,xmax,xi,wi)
+total = 0d0
+do i = 1,n
+  total = total + wi(i) * func(xi(i))
+enddo
+write(*,*) "Gauss-Legendre    :",total
+
+
+
+! Gauss-Chebyshev (first kind)
+call gche1(xmin,xmax,xi,wi)
+total = 0d0
+do i = 1,n
+  total = total + wi(i) * func(xi(i))
+enddo
+write(*,*) "Gauss-Chebyshev(1):",total
+
+
+
+! Gauss-Chebyshev (second kind)
+call gche2(xmin,xmax,xi,wi)
+total = 0d0
+do i = 1,n
+  total = total + wi(i) * func(xi(i))
+enddo
+write(*,*) "Gauss-Chebyshev(2):",total
+
+
+
+! Gauss-Laguerre
+call glag(xi,wi)
+total = 0d0
+do i = 1,n
+  if(xi(i).ge.xmin .and. xi(i).le.xmax) then
+    write(*,*) i,xi(i),wi(i)
+    total = total + wi(i) * func(xi(i))
+  endif
+enddo
+write(*,*) "Gauss-Laguerre    :",total
+
+
+
+! Exact result
+write(*,*) "Exact             :", bessel_j0(xmin) - bessel_j0(xmax)
+!write(*,*) "Exact             :", bessel_y0(xmin) - bessel_y0(xmax)
+
+
+
+contains
+!integrand function
+function func(x) result(fx)
+real(dp), intent(in) :: x
+real(dp) :: fx
+fx = bessel_j1(x)
+!fx = bessel_y1(x)
+end function func
+end program main
+
